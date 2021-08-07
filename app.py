c8547634509d7d2e27ca6120f0b9b14a1e3c70ba
liuqinh2s <liuqinh2s@gmail.com>
2021-08-07 23:17:42 +0800

    量化交易注册

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, request
+import pika
+import json
+
 app = Flask(__name__)
+
+# 创建socket实例，声明管道
+connect = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+channel = connect.channel()
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/register', methods=["POST"])
+def register():
+    apiKey = request.form.get('api-key')
+    secretKey = request.form.get('secret-key')
+    notifyUid = request.form.get('notify-uid')
+    quantity = request.form.get('quantity')
+    leverage = request.form.get('leverage')
+    contact = request.form.get('contact')
+    username = request.form.get('contact')
+
+    keys = 'register'
+    message = {"apiKey": apiKey, "secretKey": secretKey, "notifyUid": notifyUid, "quantity": quantity,
+               "leverage": leverage, 'contact': contact, 'username': username}
+    channel.basic_publish(
+        exchange="",
+        routing_key=keys,
+        body=json.dumps(message)
+    )
+    print("send %s  %s" % (keys, message))
+    return 'register success!'
+
+
+@app.route('/stop', methods=["POST"])
+def stop():
+    pass
 
 
 if __name__ == '__main__':
